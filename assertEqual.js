f6f9959f475426e6141b0f57b1e62fb5a9e31448
toddteigland <toddteigland@gmail.com>
2022-11-14 15:38:52 -0800

    Changed output string to contain template literals instead of concactination

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("👍👍👍 Assertion Passed: " + actual + " === " + expected);
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("👎👎👎 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`👎👎👎 Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
