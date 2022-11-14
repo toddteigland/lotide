ad941b3db624fe8b6bf196c1776020da28bf0626
toddteigland <toddteigland@gmail.com>
2022-11-14 15:16:51 -0800

    Created Assert function to test code in the terminal

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("👍👍👍 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("👎👎👎 Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Work from Home", 4);
+assertEqual("4",4);
