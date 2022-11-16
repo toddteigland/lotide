aeed67a6843c226358f774fe26a16358ac4200cc
toddteigland <toddteigland@gmail.com>
2022-11-16 14:08:46 -0800

    Looping through an array and checking to see if they match conditions set in an object we also loop through, then using AssertEqual to test if conditions are being met

---
+++ countOnly.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎👎👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countOnly = function(allItems, itemstoCount) {
+  const results = {};
+  for (const item of allItems) {
+    console.log(item);
+    if (itemstoCount[item]) {
+      if (results[item]) {
+      results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+
