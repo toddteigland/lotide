9253970d95ac3a045d2429fd16efbaf69887112a
toddteigland <toddteigland@gmail.com>
2022-11-15 18:45:03 -0800

    Comparing Arrays to see if they're exactly the same. Then using another assertEqual function to show if it passed or not

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎👎👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } 
+  for(var i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+assertEqual(eqArrays([3, 2, 1], [3, 2, 1]), true)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false)
