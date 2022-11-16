b6f39b571002b473e9c0236eda3f6f4d76762b66
toddteigland <toddteigland@gmail.com>
2022-11-16 15:06:27 -0800

    added comments to the bottom to explain how the funcitons were intended to work

--- middle.js
+++ middle.js
@@ -45,3 +45,6 @@ assertArraysEqual(middle([1,2,3,4]), [2,3]);
 //splice and pop first and last elements
 //if .length is odd only single element should remove
 // if .length is even two should be left
+// use assertarraysequal to test, which uses eqarrays to see if my result in the test on 
+//     line 40 match my expected also on line 40. if it does, it passes a positive back
+//     to assertarraysequal which prints out that nice message to the console
