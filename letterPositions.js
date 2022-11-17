ad9c65ae1c15d56cd3977639017431be85522b6e
toddteigland <toddteigland@gmail.com>
2022-11-16 19:20:09 -0800

    Looping through a sentence, creating an object with the letters as keys and the indexes each letter is found at as values printed as arrays

---
+++ letterPositions.js
@@ -0,0 +1,17 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === " ") {
+      continue;
+    }
+    if (!results[sentence[i]]) {
+      results[sentence[i]] = [];
+    }
+
+    results[sentence[i]].push(i);
+  }
+  return results;
+};
+
+
+console.log(letterPositions("lighthouse in the house"));
