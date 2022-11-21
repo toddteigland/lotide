576b26fcadfd36da3255568c16c280622cb74f6c
toddteigland <toddteigland@gmail.com>
2022-11-21 09:43:49 -0800

    Refactoring code to import test function (assertEqual)

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👎👎👎 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(input) {
   return input[0];
